ca56aebb8679ae606b98fcf5468d541bd26e751d
Cuteseal <cuteseal@hotmail.com>
2025-05-17 23:51:07 +1000

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # minui-dotclean-pak
 Small utility to clean the filesystem of mac "dot" files.
+
+## Requirements
+
+This pak is designed for and tested with the following MinUI Platforms and devices:
+
+- `tg5040`: Trimui Brick (formerly `tg3040`)
+
+## Installation
+
+1. Mount your MinUI SD card.
+2. Download the latest [release](https://github.com/laesetuc/minui-dotclean-pak/releases) from GitHub.
+3. Copy the zip file to the correct platform folder in the "/Tools" directory on the SD card.
+4. Extract the zip in place, then delete the zip file.
+5. Confirm that there is a `/Tools/$PLATFORM/DotClean.pak/launch.sh` file on your SD card.
+6. Unmount your SD Card and insert it into your MinUI device.
+
+Note: The platform folder name is based on the name of your device. For example, if you are using a TrimUI Brick, the folder is "tg5040". Alternatively, if you're not sure which folder to use, you can copy the .pak folders to all the platform folders.
+
+## Usage
+
+Run the utility to clean the SD Card of Mac filesystem files.
+
+## Acknowledgements
+
+- [MinUI](https://github.com/shauninman/MinUI) by Shaun Inman
+- [minui-presenter](https://github.com/josegonzalez/minui-presenter) by Jose Diaz-Gonzalez
+- Also, thank you, Jose Diaz-Gonzalez, for your pak repositories, which this project is based on.
+- [OnionUI](https://github.com/OnionUI/Onion) project for the cleanup code.
+
+## License
+
+This project is released under the MIT License. For more information, see the [LICENSE](LICENSE) file.
---
+++ launch.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+PAK_DIR="$(dirname "$0")"
+PAK_NAME="$(basename "$PAK_DIR")"
+PAK_NAME="${PAK_NAME%.*}"
+[ -f "$USERDATA_PATH/$PAK_NAME/debug" ] && set -x
+
+rm -f "$LOGS_PATH/$PAK_NAME.txt"
+exec >>"$LOGS_PATH/$PAK_NAME.txt"
+exec 2>&1
+
+echo "$0" "$@"
+cd "$PAK_DIR" || exit 1
+mkdir -p "$USERDATA_PATH/$PAK_NAME"
+
+ARCHITECTURE=arm
+if [[ "$(uname -m)" == *"64"* ]]; then
+    ARCHITECTURE=arm64
+fi
+
+export HOME="$USERDATA_PATH/$PAK_NAME"
+export LD_LIBRARY_PATH="$PAK_DIR/lib:$LD_LIBRARY_PATH"
+export PATH="$PAK_DIR/bin/$ARCHITECTURE:$PAK_DIR/bin/$PLATFORM:$PAK_DIR/bin:$PATH"
+
+show_message() {
+    message="$1"
+    seconds="$2"
+
+    if [ -z "$seconds" ]; then
+        seconds="forever"
+    fi
+
+    killall minui-presenter >/dev/null 2>&1 || true
+    echo "$message" 1>&2
+    if [ "$seconds" = "forever" ]; then
+        minui-presenter --message "$message" --timeout -1 &
+    else
+        minui-presenter --message "$message" --timeout "$seconds"
+    fi
+}
+
+cleanup() {
+    rm -f /tmp/stay_awake
+    killall minui-presenter >/dev/null 2>&1 || true
+}
+
+main() {
+    echo "1" >/tmp/stay_awake
+    trap "cleanup" EXIT INT TERM HUP QUIT
+
+    if ! command -v minui-presenter >/dev/null 2>&1; then
+        show_message "minui-presenter not found" 2
+        return 1
+    fi
+
+    show_message "Cleaning up dot files..." "forever"
+
+    cd "$SDCARD_PATH/"
+    rm -rf .Spotlight-V100 .apDisk .fseventsd .TemporaryItems .Trash .Trashes
+
+    find . -depth -type f \( -name "._*" -o -name ".DS_Store" \) -not -path "**/._state_seen/*" -delete
+    find . -depth -type d -name "__MACOSX" -exec rm -rf {} \;
+    find . -type f -name "*_cache[0-9].db" -exec rm -f {} \;
+
+    killall minui-presenter >/dev/null 2>&1 || true
+    show_message "Cleanup complete!" 2
+
+main "$@"
